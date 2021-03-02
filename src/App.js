3cf147525f32dc023565ad8961c2c62d9c2f95a2
ramzi97-tlili <tliliramzi97@gmail.com>
2021-03-02 14:25:31 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import './style.css';
+import imageInSrc from "./imageInSrc.jpg"
+
+function App() {
+  return (
+    <div>
+    <div style={{border: 'solid 1px black', maxWidth: '100vw'}}>
+      <h1 className="title red">Ramzi tlili</h1>
+      <br />
+      <img src={imageInSrc} alt ='Image' />
+      <br />
+      <img src="/imageInPublic.jpg" />
+      <br/>
+    </div>
+    <video width={320} height={240} controls>
+      <source src="myVideo.mp4" type="video/mp4" />
+    </video>
+  </div>
+  );
+}
+export default App;
